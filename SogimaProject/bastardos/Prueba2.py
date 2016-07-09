cd8909cafb77cf0da59cc899005e5134562864c6
dmartinez040 <dmartinez040@users.noreply.github.com>
2016-07-09 21:54:04 +0200

    Prueba2

---
+++ SogimaProject/bastardos/Prueba2.py
@@ -0,0 +1,17 @@
+'''
+Created on 9 jul. 2016
+
+@author: David
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
